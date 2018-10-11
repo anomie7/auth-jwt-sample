54f1be7beac22d602554410b741380f2f63d7255
Justin <anomie7777@gmail.com>
2018-10-11 15:44:25 +0900

    init

---
+++ src/main/java/com/depromeet/track4/AuthJwtPracApplication.java
@@ -0,0 +1,12 @@
+package com.depromeet.track4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtPracApplication.class, args);
+	}
+}
