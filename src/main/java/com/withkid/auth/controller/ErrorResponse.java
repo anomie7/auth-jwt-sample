c0be88866facf3b9d88ddf7263c630ef4a1b128f
Justin <anomie7777@gmail.com>
2018-11-08 15:19:44 +0900

    [Modify] Exception Handler에서 Error Response를 추가

---
+++ src/main/java/com/withkid/auth/controller/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.withkid.auth.controller;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+class ErrorResponse {
+	private String name;
+	private String msg;
+	private HttpStatus status;
+}
--- src/main/java/com/withkid/auth/controller/ExceptionController.java
+++ src/main/java/com/withkid/auth/controller/ExceptionController.java
@@ -14,19 +14,25 @@ import io.jsonwebtoken.ExpiredJwtException;
 @ControllerAdvice
 @RestController
 public class ExceptionController {
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
-	public ResponseEntity<String> userNotFoundHanler(){
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저를 찾을 수 없습니다.");
+	public ResponseEntity<ErrorResponse> userNotFoundHanler() {
+		ErrorResponse body = ErrorResponse.builder().name(UserNotFoundException.class.getSimpleName())
+				.msg("유저를 찾을 수 없습니다.").status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
 	}
-	
+
 	@ExceptionHandler(ExpiredJwtException.class)
-	public ResponseEntity<String> tokenExpiredHanler(){ 
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰의 유효기간이 초과했습니다.");
+	public ResponseEntity<ErrorResponse> tokenExpiredHanler() {
+		ErrorResponse body = ErrorResponse.builder().name(ExpiredJwtException.class.getSimpleName())
+				.msg("토큰의 유효기간이 초과했습니다.").status(HttpStatus.UNAUTHORIZED).build();
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
 	}
-	
+
 	@ExceptionHandler(JwtTypeNotMatchedException.class)
-	public ResponseEntity<String> tokenTypeNotMatchedHanler(){ 
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰의 타입이 불일치합니다.");
+	public ResponseEntity<ErrorResponse> tokenTypeNotMatchedHanler() {
+		ErrorResponse body = ErrorResponse.builder().name(JwtTypeNotMatchedException.class.getSimpleName())
+				.msg("토큰의 타입이 불일치합니다.").status(HttpStatus.UNAUTHORIZED).build();
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
 	}
 }
