67edd0dc9b9601be8e4e99022396d9e8602aebb2
Justin <anomie7777@gmail.com>
2018-11-16 19:16:21 +0900

    [Modify] cor 설정 변경

--- src/main/java/com/withkid/auth/cofig/WebConfig.java
+++ src/main/java/com/withkid/auth/cofig/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").exposedHeaders("Authorization", "refresh-token");
+		registry.addMapping("/*").allowedOrigins("*").exposedHeaders("Authorization", "refresh-token");
 	}
 }
