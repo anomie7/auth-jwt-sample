d1170cb13e962c25f3c8395e54c0dfdb14d1189f
Justin <anomie7777@gmail.com>
2019-02-11 22:59:21 +0900

    [impl] ShaUtill에 대한 테스트 케이스 작성

---
+++ src/test/java/com/withkid/auth/utills/TestShaUtill.java
@@ -0,0 +1,28 @@
+package com.withkid.auth.utills;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class TestShaUtill {
+
+    @Test
+    public void sha256() throws NoSuchAlgorithmException {
+        String password = "1234";
+        String result = ShaUtill.sha256(password);
+
+        System.out.println(result);
+        assertEquals(false, result.equals(password));
+    }
+
+
+}
