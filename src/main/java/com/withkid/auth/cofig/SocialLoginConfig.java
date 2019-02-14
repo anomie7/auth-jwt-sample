9719a4d6f7520f88bbc8a612b96aa1bb82cb292c
Justin <anomie7777@gmail.com>
2019-02-14 14:59:43 +0900

    [add java config] 소셜 로그인을 위한 객체를 스프링 빈으로 등록하는 Java configuration 생성

---
+++ src/main/java/com/withkid/auth/cofig/SocialLoginConfig.java
@@ -0,0 +1,29 @@
+package com.withkid.auth.cofig;
+
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.OAuth2Operations;
+
+@Configuration
+public class SocialLoginConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public FacebookConnectionFactory facebookConnectionFactory(){
+        String facebookAppId = env.getProperty("spring.social.facebook.app-id");
+        String facebookSecret = env.getProperty("spring.social.facebook.app-secret");
+        FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
+        return connectionFactory;
+    }
+
+    @Bean
+    public OAuth2Operations oAuth2Operations(){
+        return facebookConnectionFactory().getOAuthOperations();
+    }
+}
